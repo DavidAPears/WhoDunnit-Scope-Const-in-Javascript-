2369b515c048c955afff19c5f9266a2e202f02e6
David A Pears <davidapears@gmail.com>
2018-07-30 15:30:49 +0100

    Initial Commit

---
+++ Episode1.js
@@ -0,0 +1,31 @@
+// # Scope: Who Dunnit
+//
+// ### Learning Objectives
+//
+// - Understand function scope
+// - Know the difference in between the let and const keywords
+//
+// ## Brief
+//
+// Using your knowledge about scope and variable declarations in JavaScript, look at the following code snippets and predict what the output or error will be and why.
+
+
+
+// #### Episode 1
+
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Library',
+  weapon: 'Rope'
+};
+
+const declareMurderer = function() {
+  return `The murderer is ${scenario.murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+
+
+// What I expected to see:
+// `The murderer is Miss Scarlet'
